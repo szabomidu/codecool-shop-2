1a795a9073826f24a4493f7349ba8599c1ccee8e
bodroghykristof <bodroghykristof@gmail.com>
2020-10-13 20:24:15 +0200

    Display pictures on website

--- src/main/webapp/static/js/dom.js
+++ src/main/webapp/static/js/dom.js
@@ -34,7 +34,7 @@ function displayFilterResults(response) {
             newContent +=
                 `<div class="col col-sm-12 col-md-6 col-lg-4">
                     <div class="card">
-                        <img class="" src='/static/img/product_1.jpg' alt=""/>
+                        <img class="" src='/static/img/product_${product.id}.jpg' alt=""/>
                         <div class="card-header">
                             <h4 class="card-title">${product.name}</h4>
                             <p class="card-text">${product.description}</p>
