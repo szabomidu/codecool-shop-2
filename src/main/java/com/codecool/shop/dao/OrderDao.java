b8f9da8940cbe75f8a12acea824828897a99a061
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-10-14 10:44:07 +0200

    Method add now returns int (order id)

--- src/main/java/com/codecool/shop/dao/OrderDao.java
+++ src/main/java/com/codecool/shop/dao/OrderDao.java
@@ -5,7 +5,7 @@ import com.codecool.shop.model.Order;
 import java.util.List;
 
 public interface OrderDao {
-    void add(Order order);
+    int add(Order order);
     void addAll(Order ... orders);
     Order find(int id);
     void remove(int id);
