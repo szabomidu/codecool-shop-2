aaa24c77aa1c2cb038eff6b91d75ce212d678c75
reClicked <gamper.renato@gmail.com>
2020-10-15 12:05:12 +0200

    Get order items and send them to template

--- src/main/java/com/codecool/shop/controller/CheckOutController.java
+++ src/main/java/com/codecool/shop/controller/CheckOutController.java
@@ -1,6 +1,8 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -13,16 +15,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/confirm")
+@WebServlet(urlPatterns = "/checkout")
 public class CheckOutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BufferedReader reader = req.getReader();
+        OrderDao orderData = OrderDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        engine.process("checkout/confirm.html", context, resp.getWriter());
+        int orderId = Integer.parseInt(req.getParameter("id"));
+        context.setVariable("lineitems", orderData.find(orderId));
+
+        engine.process("checkout/checkout.html", context, resp.getWriter());
 
     }
 }
