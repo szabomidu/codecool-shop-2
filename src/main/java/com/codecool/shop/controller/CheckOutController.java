44f510fbeacc27ace71b8cf7df9f7cf065a2d5d1
reClicked <gamper.renato@gmail.com>
2020-10-15 10:30:11 +0200

    Create CheckOutController

---
+++ src/main/java/com/codecool/shop/controller/CheckOutController.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/api/confirm")
+public class CheckOutController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        engine.process("checkout/confirm.html", context, resp.getWriter());
+
+    }
+}
