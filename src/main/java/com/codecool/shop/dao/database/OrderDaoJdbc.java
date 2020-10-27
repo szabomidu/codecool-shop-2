960fb504b448d2526515781420636a8eab2424c2
bodroghykristof <bodroghykristof@gmail.com>
2020-10-27 14:40:48 +0100

    Implement finding Order by id

--- src/main/java/com/codecool/shop/dao/database/OrderDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/database/OrderDaoJdbc.java
@@ -1,10 +1,11 @@
 package com.codecool.shop.dao.database;
 
 import com.codecool.shop.dao.OrderDao;
-import com.codecool.shop.model.Order;
+import com.codecool.shop.model.*;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class OrderDaoJdbc implements OrderDao {
@@ -33,12 +34,27 @@ public class OrderDaoJdbc implements OrderDao {
 
     @Override
     public void addAll(Order... orders) {
-
+        Arrays.stream(orders).forEach(this::add);
     }
 
     @Override
     public Order find(int id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, user_id, first_name, last_name, email, address, zip_code, city, country FROM order WHERE id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (!rs.next()) {
+                return null;
+            }
+            Order order = new Order(rs.getInt(2));
+            order.setId(id);
+            OrderData orderData = new OrderData(rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getInt(7), rs.getString(8), rs.getString(9));
+            order.saveData(orderData);
+            return order;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while finding Product.", e);
+        }
     }
 
     @Override
