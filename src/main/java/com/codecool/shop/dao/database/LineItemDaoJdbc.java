6b90041a6aa5b4e1dd99e4c05aee4d7c507bf576
reClicked <gamper.renato@gmail.com>
2020-10-27 14:14:26 +0100

    Create LineItemDaoJdbc class

---
+++ src/main/java/com/codecool/shop/dao/database/LineItemDaoJdbc.java
@@ -0,0 +1,33 @@
+package com.codecool.shop.dao.database;
+
+import com.codecool.shop.dao.LineItemDao;
+import com.codecool.shop.model.LineItem;
+
+import java.util.List;
+
+public class LineItemDaoJdbc implements LineItemDao {
+    @Override
+    public void add(LineItem lineItem) {
+
+    }
+
+    @Override
+    public void addAll(LineItem... lineItems) {
+
+    }
+
+    @Override
+    public LineItem find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<LineItem> getAll() {
+        return null;
+    }
+}
