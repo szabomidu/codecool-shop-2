5b0c101bd6d3b73e7d847bf922bf7c2f74a2d167
bodroghykristof <bodroghykristof@gmail.com>
2020-10-27 10:16:05 +0100

    Add categories to database

---
+++ sample_data/sample_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO product_category (name, description) VALUES ('Wizard', 'Magical guy with wand or walking stick');
+INSERT INTO product_category (name, description) VALUES ('Muggle', 'Non-magical guy without wand or walking stick');
+INSERT INTO product_category (name, description) VALUES ('Hobbit', 'Hairy soled cute little alcoholic creature');
+INSERT INTO product_category (name, description) VALUES ('Elf', 'Guy with pointy ears');
+INSERT INTO product_category (name, description) VALUES ('Dwarf', 'Stuffy hairy caveman');
+INSERT INTO product_category (name, description) VALUES ('Jedi', 'Cloaked man with a colorful dingdong');
+INSERT INTO product_category (name, description) VALUES ('Sith', 'Evil cloaked man with a red dingdong');
+INSERT INTO product_category (name, description) VALUES ('Human', 'Not much really');
+INSERT INTO product_category (name, description) VALUES ('Dragon', 'Fire-breathing winged pet');
+INSERT INTO product_category (name, description) VALUES ('Dark Lord', 'Attention ladies! This is the group of bad guyz');
