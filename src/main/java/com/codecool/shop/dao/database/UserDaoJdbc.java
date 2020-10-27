a15379b5a3394a0c71f7669a160610cbfad83065
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-10-27 14:39:05 +0100

    Implemented method getBy

--- src/main/java/com/codecool/shop/dao/database/UserDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/database/UserDaoJdbc.java
@@ -89,6 +89,6 @@ public class UserDaoJdbc implements UserDao {
 
     @Override
     public User getBy(Order order) {
-        return null;
+        return null;//find(order.getUserId()); TODO: uncomment these lines
     }
 }
