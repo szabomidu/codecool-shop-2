ac18980c9b35522fb9596e98f0b3a837585f3d96
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-10-28 11:35:21 +0100

    Created setup method

--- src/test/java/com/codecool/shop/dao/memory/ProductDaoMemTest.java
+++ src/test/java/com/codecool/shop/dao/memory/ProductDaoMemTest.java
@@ -1,13 +1,29 @@
 package com.codecool.shop.dao.memory;
 
+import com.codecool.shop.model.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 class ProductDaoMemTest {
+    private List<Product> data;
+    private ProductDaoMem productDaoMem;
+    @Mock ProductDaoMem mockProductDaoMem = Mockito.mock(ProductDaoMem.class);
+    @Mock Product product = Mockito.mock(Product.class, Mockito.RETURNS_DEEP_STUBS);
 
     @BeforeEach
     void setUp() {
+        data = new ArrayList<>();
+        productDaoMem = ProductDaoMem.getInstance();
+
     }
 
     @Test
