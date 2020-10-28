4004a970d923808d9491d17bb7f7253cd76dd2c7
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-10-28 10:17:49 +0100

    Created test file ProductDaoMem

---
+++ src/test/java/com/codecool/shop/dao/memory/ProductDaoMemTest.java
@@ -0,0 +1,48 @@
+package com.codecool.shop.dao.memory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class ProductDaoMemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getBy() {
+    }
+
+    @Test
+    void testGetBy() {
+    }
+
+    @Test
+    void testGetBy1() {
+    }
+}
