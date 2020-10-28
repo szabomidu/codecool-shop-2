b65b72563d3d0c2b8576e2515b160b2a3a8bf27d
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-10-28 11:36:25 +0100

    Added method add_addProduct_setsProperIdToProduct to test whether the method sets the proper id to the added item

--- src/test/java/com/codecool/shop/dao/memory/ProductDaoMemTest.java
+++ src/test/java/com/codecool/shop/dao/memory/ProductDaoMemTest.java
@@ -39,6 +39,14 @@ class ProductDaoMemTest {
     }
 
     @Test
+    void add_addProduct_setsProperIdToProduct() {
+        productDaoMem.add(product);
+        doCallRealMethod().when(product).setId(data.size() + 1);
+        Assertions.assertEquals(product.getId(),  data.size());
+    }
+
+
+    @Test
     void addAll() {
     }
 
