8ddcb49d3b685e3b10a60790ac9d08c2b90763ed
reClicked <gamper.renato@gmail.com>
2020-10-29 13:15:49 +0100

    Edit test methods so they are not dependent on each other

--- src/main/Test/com/codecool/shop/dao/memory/UserDaoMemTest.java
+++ src/main/Test/com/codecool/shop/dao/memory/UserDaoMemTest.java
@@ -4,67 +4,61 @@ import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.model.User;
 import com.codecool.shop.model.Order;
 import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserDaoMemTest {
 
     private static UserDao userDao;
-    private static Order order1;
-    private static User user1;
-    private static User user2;
-    private static User user3;
+    private static final Order order = new Order();
+    private static final User user1 = new User("user1");
+    private static final User user2 = new User("user2");
+    private static final User user3 = new User("user3");
 
-    @BeforeAll
-    static void setup() {
+
+    @BeforeEach
+    void setup() {
         userDao = UserDaoMem.getInstance();
-        order1 = mock(Order.class);
-        user1 = mock(User.class);
-        user2 = mock(User.class);
-        user3 = mock(User.class);
+        userDao.add(user1);
+    }
+
+    @AfterEach
+    void clearUsers() {
+        userDao.clearData();
     }
 
     @Test
-    @org.junit.jupiter.api.Order(1)
     void add() {
-        userDao.add(user1);
         assertEquals(1, userDao.getAll().size());
     }
 
     @Test
-    @org.junit.jupiter.api.Order(2)
     void addAll() {
         userDao.addAll(user2, user3);
         assertEquals(3, userDao.getAll().size());
     }
 
     @Test
-    @org.junit.jupiter.api.Order(3)
     void find() {
-        User expectedUser = userDao.find(0);
+        User expectedUser = userDao.find(1);
         assertEquals(user1, expectedUser);
     }
 
     @Test
-    @org.junit.jupiter.api.Order(4)
     void remove() {
-        userDao.remove(0);
-        assertEquals(2, userDao.getAll().size());
+        userDao.remove(1);
+        assertEquals(0, userDao.getAll().size());
     }
 
     @Test
-    @org.junit.jupiter.api.Order(5)
     void getAll() {
-        assertEquals(2, userDao.getAll().size());
+        assertEquals(1, userDao.getAll().size());
     }
 
     @Test
-    @org.junit.jupiter.api.Order(6)
     void getBy() {
+        user1.addOrder(order);
+        assertEquals(user1, userDao.getBy(order));
     }
 }
