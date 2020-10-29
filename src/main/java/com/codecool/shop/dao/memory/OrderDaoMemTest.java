33bded7c0631d81a1697437faa77f16bbe2200d8
Szkini22 <tamas.catanzaro98@gmail.com>
2020-10-29 15:15:48 +0100

    tests implemented for orderDaoMem

--- src/main/java/com/codecool/shop/dao/memory/OrderDaoMemTest.java
+++ src/main/java/com/codecool/shop/dao/memory/OrderDaoMemTest.java
@@ -1,55 +1,79 @@
 package com.codecool.shop.dao.memory;
 
+import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.model.Order;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class OrderDaoMemTest {
-    List<Order> data = new ArrayList<>();
+    private static OrderDao orderDao;
+    private static final Order order1 = new Order();
+    private static final Order order2 = new Order();
+    private static final Order order3 = new Order();
 
-    @Test
-    void getInstance() {
+    @BeforeEach
+    void setup() {
+        orderDao = OrderDaoMem.getInstance();
+        orderDao.add(order1);
+    }
+
+    @AfterEach
+    void clearOrders() {
+        orderDao.clearData();
     }
 
     @Test
-    void addOrderToMemReturnsTheProperSizeTest() {
-        Order order = new Order();
-        Order order1 = new Order();
-        Order order2 = new Order();
-        data.add(order);
-        data.add(order1);
-        data.add(order2);
-        assertEquals(data.size(), 3);
-
+    @org.junit.jupiter.api.Order(1)
+    void add() {
+        orderDao.add(order1);
+        assertEquals(2, orderDao.getAll().size());
     }
 
     @Test
-    void addOrderToMemReturnsTheOrderId() {
-        Order order = new Order();
-        order.setId(data.size() + 1);
-        assertEquals(order.getId(), 1);
+    @org.junit.jupiter.api.Order(6)
+    void addMethodSetsTheProperIdForOrder() {
+        int expectedId = 1;
+        assertEquals(expectedId, orderDao.find(1).getId());
     }
 
+
     @Test
+    @org.junit.jupiter.api.Order(2)
     void addAll() {
-
+        orderDao.addAll(order2, order3);
+        assertEquals(3, orderDao.getAll().size());
     }
 
     @Test
+    @org.junit.jupiter.api.Order(3)
     void find() {
+        Order expectedOrder = orderDao.find(1);
+        assertEquals(order1, expectedOrder);
     }
 
     @Test
+    @org.junit.jupiter.api.Order(7)
+    void findOrderThatDoesntExistsReturnNull() {
+        Order expectedOrder = orderDao.find(2);
+        assertNull(expectedOrder);
+    }
+
+    @Test
+    @org.junit.jupiter.api.Order(4)
     void remove() {
+        orderDao.remove(1);
+        assertEquals(0, orderDao.getAll().size());
     }
 
     @Test
+    @org.junit.jupiter.api.Order(5)
     void getAll() {
+        assertEquals(1, orderDao.getAll().size());
     }
+
 }
